113cccdab6e16943031a3de4ba4393bd25a8ed96
Vinicius <vinicius_samy@outlook.com>
2024-06-29 12:08:15 -0300

    Retirando interatividade de install_docker_engine.sh

--- install_docker_engine.sh
+++ install_docker_engine.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -15,7 +15,7 @@ echo \
 sudo apt-get update
 
 # Install the Docker packages:
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Adding user on docker group
 HOME_USER=$USER
